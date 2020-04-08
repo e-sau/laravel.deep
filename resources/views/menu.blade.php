3916794bfe6ea90f8f5afd69e69f2005331931c4
Maxim Borodin <esau@yandex.ru>
2020-04-08 20:29:11 +0300

    base menu

--- resources/views/menu.blade.php
+++ resources/views/menu.blade.php
@@ -1,31 +1,29 @@
 @section('menu')
-    <nav class="navbar-expand navbar navbar-dark bg-dark justify-content-end mb-5">
-        <ul class="navbar-nav">
-            <li class="nav-item">
-                <a class="nav-link @if(Route::currentRouteName() === 'home'){{ "active" }}@endif"
+    <nav class="navbar navbar-dark bg-dark mb-5">
+        <div class="dropdown">
+            <button class="navbar-toggler" id="dropdownMenuButton" type="button" data-toggle="dropdown">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                <a class="dropdown-item @if(Route::currentRouteName() === 'home'){{ "active" }}@endif"
                    href="{{ route('home') }}">@lang('menu.home')</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link @if(Route::currentRouteName() == 'about'){{ "active" }}@endif"
+                <a class="dropdown-item  @if(Route::currentRouteName() == 'about'){{ "active" }}@endif"
                    href="{{ route('about') }}">@lang('menu.about')</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link
+                <a class="dropdown-item
                     @if(Route::currentRouteName() == 'news.category.index'
                     || Route::currentRouteName() == 'news.show'){{ "active" }}@endif"
                    href="{{ route('news.category.index') }}">@lang('menu.news')</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link
-                    @if(Route::currentRouteName() == 'auth.index'){{ "active" }}@endif"
-                   href=
-                   @if(!session('auth'))
-                        "{{ route('auth.index') }}">@lang('menu.login')
-                    @else
-                        "{{ route('auth.logout') }}">@lang('menu.logout')
-                   @endif
-                </a>
-            </li>
-        </ul>
+            </div>
+        </div>
+        <div class="left-menu">
+            <a class="btn btn-outline-info my-2 my-sm-0"
+               href=
+               @if(!session('auth'))
+                   "{{ route('auth.index') }}">@lang('menu.login')
+                @else
+                    "{{ route('auth.logout') }}">@lang('menu.logout')
+                @endif
+            </a>
+        </div>
     </nav>
 @endsection
